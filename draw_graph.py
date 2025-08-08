2987beb23855c759c685d60f22dd3d88cb7e7a9b
Em-kale <emmittluhning@outlook.com>
2025-08-07 21:05:41 -0400

    added a start for the attractive spring force calculation

--- draw_graph.py
+++ draw_graph.py
@@ -1,13 +1,20 @@
 
 import numpy as np
 
+# Graph constants
 UPPER_BOUND = 10
 NUMBER_OF_NODES = 4
 
+# Force directed algorithm constants
 FORCE_THRESHOLD = 1
 MAX_ITERATIONS = 1
 REPULSION_CONSTANT = 2.0
 MIN_DISTANCE = 0.05
+
+# For total area of canvas
+HEIGHT = 20
+WIDTH = 20
+
 # our graph
 adjacencyMatrix = np.empty((NUMBER_OF_NODES, NUMBER_OF_NODES))
 
@@ -36,8 +43,38 @@ def calculate_positions():
 
 
 def get_attractive_sum(node):
+    spring_force_sum = np.array([0, 0, 0])
     print("calculating attractive force...")
-    # calculate attractive force
+    # shoule only sum for nodes that are connected of course
+    # TODO: add that
+    for i in layout:
+        if i is node:
+            continue
+
+        euclidian_distance = np.linalg.norm(i-node)
+
+        if euclidian_distance < MIN_DISTANCE:
+            euclidian_distance = MIN_DISTANCE
+        # calclate the unit vector between i and node , from i -> node
+        vector_between_nodes = node - i
+
+        # calculate unit vector (same direction as vector
+        # between nodes but length 1)
+        unit_vector = vector_between_nodes / euclidian_distance
+
+        # calculate the ideal edge length
+        # according to a variant of eade, the fruchterman-reingold algorithm
+        # this should be the square root of area / number of nodes
+        area = WIDTH * HEIGHT
+        ideal_length = np.sqrt((area / NUMBER_OF_NODES))
+
+        spring_force = np.log(euclidian_distance / ideal_length)*unit_vector
+        spring_force_sum += spring_force
+
+    # TODO: the lecture on this mentions something about subtracting
+    # the repulsive force for these nodes. look into that
+    # if you get a bad result
+    return spring_force_sum
 
 
 def get_repulsive_sum(node):
@@ -55,7 +92,8 @@ def get_repulsive_sum(node):
         # calclate the unit vector between i and node , from i -> node
         vector_between_nodes = node - i
 
-        # calculate unit vector (same direction as vector between nodes but length 1)
+        # calculate unit vector (same direction as vector
+        # between nodes but length 1)
         unit_vector = vector_between_nodes / euclidian_distance
 
         # calculate repulsive force
